9667d2b692090cd4c84d29f4c5f85ba15165f193
clecle253 <clement.cazaubon@epitech.eu>
2018-05-25 16:57:01 +0200

    [wip]

--- client_src/main.c
+++ client_src/main.c
@@ -6,6 +6,11 @@
 */
 
 #include <ncurses.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/client.h"
 
 void	write_separator()
 {
@@ -17,9 +22,19 @@ void	write_separator()
 		i++;
 	}
 }
+char *getmessage()
+{
+	static char message[515];
+
+	read(0,message, 515);
+	return message;
+}
 
 int main(int ac, char **av)
 {
+	if (ac != 2)
+		return 84;
+
 	initscr();
 	write_separator();
 	move(LINES -1,0);
