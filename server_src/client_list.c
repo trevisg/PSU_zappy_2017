6309e1e62f020b66b6c4070fc4c10be658106f27
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-26 01:56:26 +0200

    updates on server list chained list methods

--- server_src/client_list.c
+++ server_src/client_list.c
@@ -6,15 +6,54 @@
 */
 
 #include "../include/server.h"
+#include <stdlib.h>
 
-t_user *get_new_user(int clifd, int mode, char *nick, char *rname)
+t_user *get_new_user(int clifd, cmdargs usercmd)
 {
-	t_user usr = malloc(sizeof(*usr));
+	t_user *usr = malloc(sizeof(*usr));
 
 	if (usr) {
+		memset(usr, 0, sizeof(*usr));
 		usr->clifd = clifd;
-		usr->mode = mode;
-		usr->nick = nick;
-		usr->rname = rname;
+		usr->mode = atoi(usercmd[2]);
+		usr->nick = strdup(usercmd[1]);
+		usr->rname = strdup(usercmd[4]);
+	}
+	return (usr);
+}
+
+t_userlist *get_new_userlist(t_user *usr)
+{
+	t_userlist *list = malloc(sizeof(*list));
+
+	if (list) {
+		memset(list, 0, sizeof(*list));
+		list->user = usr;
+		list->prev = NULL;
+		list->next = NULL;
+	}
+	return (list);
+}
+
+void *push_back(t_userlist *head, t_userlist *new)
+{
+	t_userlist *tmp = head;
+
+	if (!head) {
+		head = new;
+	} else {
+		while (tmp->next) tmp = tmp->next;
+		tmp->next = new;
+		new->prev = tmp;
+	}
+	return (head);
+}
+
+void	print_users(t_userlist *liste)
+{
+	printf("\t\tUSERS LIST :\n");
+	for (t_userlist *tmp = liste; tmp; tmp = tmp->next) {
+		printf("User : [%s], Real Name : [%s]\n",
+		tmp->user->nick, tmp->user->rname);
 	}
 }
