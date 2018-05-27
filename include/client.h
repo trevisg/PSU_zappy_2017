ef953bd6f5cc949782b7021eb5dc0ecb7d32f3c8
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-27 11:30:31 +0200

    [norm] add info in header to avoid invalid header and add include guard

--- include/client.h
+++ include/client.h
@@ -1,12 +1,16 @@
 /*
 ** EPITECH PROJECT, 2018
-**
+** MY_IRC
 ** File description:
-**
+** my_irc client header file
 */
 
-struct client_data
-{
-        int epoll_fd
-        char[10]
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+struct client_data {
+        int epoll_fd;
+        char foo[10];
 };
+
+#endif /* !CLIENT_H_ */
