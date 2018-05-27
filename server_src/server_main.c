7838e1dfae55e5552660d76eea8c84750cd364ca
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-27 08:07:29 +0200

    remove command parsing from server main to avoid norm error

--- server_src/server_main.c
+++ server_src/server_main.c
@@ -13,38 +13,6 @@
 #include <arpa/inet.h>
 #include <regex.h>
 
-/** Match all methods in G_CMDS for a given req command string
-* @param req the requested command on format 'CMD <space> [ARGS]'
-* @param clifd the client socket fd
-*/
-static int	get_methods(char *req, int clifd)
-{
-	int index = 0;
-	int i = 0;
-	int j = 0;
-	char *str, *arg = NULL;
-	char args[MAXARGS][MAXARGSIZE] = {{0},{0}};
-
-	str = strdup(req);
-	arg = strtok(str, " \r\n");
-	while (arg) {
-		strncpy(args[i++], arg, MAXARGSIZE);
-		arg = strtok(NULL, " \r\n");
-	}
-	print_arg(args);
-	for (j = 0; args[j]; ++j) {
-		for (index = 0; G_PROTOS[index]; ++index) {
-			if ((strcasestr(args[j], G_PROTOS[index]))) {
-				index = G_CMDS[index](args, clifd);
-				break;
-			}
-		}
-	}
-
-	memset(req, 0, BUF_SIZE);
-	return ((index == REF_NB) ? (0) : (index));
-}
-
 /** Set the new accept() client socket in epoll event list
 * for new client handling and write 220 + [server header] on client socket
 * if connection succeed
