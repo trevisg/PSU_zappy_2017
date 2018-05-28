aae2c76668105497f16b8a99f88fb1d18cf5919f
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-28 15:22:40 +0200

    remove debug logging and add add user to chan function

--- server_src/lists_helpers.c
+++ server_src/lists_helpers.c
@@ -19,7 +19,6 @@ t_user	*find_user_by_fd(t_userlist *list, int clifd)
 
 	for (tmp = list; tmp; tmp = tmp->next) {
 		if (tmp->user->clifd == clifd) {
-			printf("%s Find \n", tmp->user->nick);
 			break;
 		}
 	}
@@ -31,7 +30,7 @@ t_user	*find_user_by_fd(t_userlist *list, int clifd)
 * @param channame the key to find the channel
 * @return res the needed userlist if found , NULL otherwise
 */
-t_userlist *get_channel_userlist(t_channel *list, char *channame)
+t_userlist	*get_channel_userlist(t_channel *list, char *channame)
 {
 	t_channel *tmp;
 	t_userlist *res = NULL;
@@ -43,4 +42,37 @@ t_userlist *get_channel_userlist(t_channel *list, char *channame)
 		}
 	}
 	return (res);
+}
+
+/** Get the t_channel pointer by its name
+* @param list the channel list
+* @param channame the resquested channel
+* @return a t_channel pointer if exists , NULL otherwise
+*/
+t_channel	*get_chan_by_name(t_channel *list, char *channame)
+{
+	t_channel *rt = NULL;
+
+	for (t_channel *tmp = list; tmp; tmp = tmp->next) {
+		if (!strcmp(tmp->channame, channame)) {
+			rt = tmp;
+			break;
+		}
+	}
+	return (rt);
+}
+
+/** Add user to channel
+* @param chanlist the channel list
+* @param userlist the userlist
+* @param user the t_user pointer to insert
+* @param user the user to put in channel
+* @return 0 if operation succeed
+* @return 1 if failure
+*/
+int	add_user_to_chan(t_channel *chanlist, t_user *usr)
+{
+	t_userlist *newuser = get_new_userlist(usr);
+	insert_back_user(chanlist->users, newuser);
+	return (newuser ? 0 : 1);
 }
