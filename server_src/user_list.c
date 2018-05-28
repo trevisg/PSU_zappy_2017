efaeb226835d819fa85d649fdc0ede681879189d
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-28 15:31:26 +0200

    update documentation

--- server_src/user_list.c
+++ server_src/user_list.c
@@ -8,6 +8,11 @@
 #include "../include/server.h"
 #include <stdlib.h>
 
+/** Malloc and memset a new t_user element
+* @param clifd the user client file descriptor
+* @param usercmd the USER <space> args 2d array
+* @return list the new element
+*/
 t_user	*get_new_user(int clifd, cmdargs usercmd)
 {
 	t_user *usr = malloc(sizeof(*usr));
@@ -22,6 +27,10 @@ t_user	*get_new_user(int clifd, cmdargs usercmd)
 	return (usr);
 }
 
+/** Malloc and memset a new userlist element
+* @param usr the t_user on the userlist
+* @return list the new element
+*/
 t_userlist	*get_new_userlist(t_user *usr)
 {
 	t_userlist *list = malloc(sizeof(*list));
@@ -35,20 +44,30 @@ t_userlist	*get_new_userlist(t_user *usr)
 	return (list);
 }
 
-void	*insert_back_user(t_userlist *head, t_userlist *new)
+/** Push back a n(ew)user in the userlist head
+* @param head the userlist to be modified
+* @param nuser the n(ew)user to be inserted
+* @return head the head pointer
+*/
+void	*insert_back_user(t_userlist *head, t_userlist *nuser)
 {
 	t_userlist *tmp = head;
 
 	if (!head) {
-		head = new;
+		head = nuser;
 	} else {
 		while (tmp->next) tmp = tmp->next;
-		tmp->next = new;
-		new->prev = tmp;
+		tmp->next = nuser;
+		nuser->prev = tmp;
 	}
 	return (head);
 }
 
+/** Remove a user from a userlist
+* @param list the userlist to be updated
+* @param clifd the clientfd as index for element to be removed
+* @return list the updated userlist
+*/
 void	*remove_user(t_userlist *list, int clifd)
 {
 	t_userlist *tmp = list;
@@ -69,6 +88,9 @@ void	*remove_user(t_userlist *list, int clifd)
 	return (list);
 }
 
+/** Self explanatory
+* @param list the list to be freed
+*/
 void	free_userlist(t_userlist *list)
 {
 	while (list) {
