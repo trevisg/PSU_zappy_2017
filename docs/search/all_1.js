482d975f597607a980595b16b5d908808a5aeb14
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-30 00:54:22 +0200

    [doc]Updates

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -4,5 +4,6 @@ var searchData=
   ['broadcast_5fmsg',['broadcast_msg',['../rfc__cmds1_8c.html#aaf538656f62d920ed911f41ea4a0f186',1,'rfc_cmds1.c']]],
   ['buf',['buf',['../structt__serv.html#a005714cf56aaffce970916774cf2b04a',1,'t_serv']]],
   ['buf_5fsize',['BUF_SIZE',['../server_8h.html#a6821bafc3c88dfb2e433a095df9940c6',1,'server.h']]],
-  ['brokenirc',['BrokenIRC',['../index.html',1,'']]]
+  ['brokenirc',['BrokenIRC',['../index.html',1,'']]],
+  ['brokenirc_20rev_200_2e43_20rfc_20write_20up',['BrokenIRC rev 0.43 RFC Write Up',['../md_bonus_RFC2812Markdown.html',1,'']]]
 ];
--- docs/search/files_3.js
+++ docs/search/files_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
+  ['rfc2812markdown_2emd',['RFC2812Markdown.md',['../RFC2812Markdown_8md.html',1,'']]],
   ['rfc_5fcmds0_2ec',['rfc_cmds0.c',['../rfc__cmds0_8c.html',1,'']]],
   ['rfc_5fcmds1_2ec',['rfc_cmds1.c',['../rfc__cmds1_8c.html',1,'']]]
 ];
