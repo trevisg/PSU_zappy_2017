22cfc19fe9ea25f00acddb3c3ac9fa6d4b51c311
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-29 10:01:51 +0200

    now fully functionnal JOIN command and minor refactoring

--- server_src/rfc_cmds0.c
+++ server_src/rfc_cmds0.c
@@ -11,16 +11,20 @@
 
 void	*join(cmdargs args, int clifd, t_channel *chans)
 {
-	t_user *user = find_user_by_fd(chans->users, clifd);
-	t_channel *reqchan = get_chan_by_name(chans, args[1]);
-	t_userlist *userlist = reqchan ? reqchan->users :
-		get_new_userlist(user);
-	t_channel *nchan = reqchan ? NULL :
-		get_new_chan_list(userlist, args[1]);
-	chans = nchan ? insert_back_channel(chans, nchan) :
-		chans;
-	dprintf(clifd, ":%s JOIN %s\r\n", user->nick, args[1]);
-	dprintf(clifd, ":127.0.0.1 331\r\n%s: no topic is set\r\n", args[1]);
+	t_user *usr = find_user_by_fd(chans->users, clifd);
+	t_userlist *list = get_new_userlist(usr);
+	t_channel *rchan = get_chan_by_name(chans, args[1]);
+
+	if (!rchan) {
+		rchan = get_new_chan_list(list, args[1]);
+		chans = insert_back_channel(chans, rchan);
+	} else if (!find_user_by_fd(rchan->users, clifd)) {
+		rchan->users = insert_back_user(rchan->users, list);
+	}
+	for (t_userlist *b = rchan->users; b; b = b->next) {
+		dprintf(b->user->clifd, ":%s JOIN :%s\r\n", usr->nick, args[1]);
+	}
+	dprintf(clifd, ":server 331\r\n%s: no topic is set\r\n", args[1]);
 	return (chans);
 }
 
@@ -51,10 +55,11 @@ void     *user(cmdargs args, int clifd, t_channel *chans)
 
 void	*quit(cmdargs args, int clifd, t_channel *chans)
 {
-	if (chans || !chans)
-	printf("[%s] cmd received\n", args[0]);
-	chans = remove_user(chans->users, clifd);
-	dprintf(clifd, ":BrokenIRC!127.0.0.1 <user> QUIT :%s", args[1]);
+	t_user *usr = find_user_by_fd(chans->users, clifd);
+	for (t_userlist *b = chans->users; b; b = b->next) {
+		dprintf(b->user->clifd, ":%s QUIT :%s", usr->nick, args[1]);
+	}
+	chans->users = remove_user(chans->users, clifd);
 	close(clifd);
 	return (chans);
 }
