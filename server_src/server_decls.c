24a3ee00e316b93521598f9c9996a78711f92b8e
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-29 11:45:07 +0200

    add documentation

--- server_src/server_decls.c
+++ server_src/server_decls.c
@@ -8,11 +8,13 @@
 
 #include "../include/server.h"
 
+/** The 2D array containing the handled commands strings for comparison  */
 const char *G_PROTOS[REF_NB] = {
 	"JOIN", "NICK", "PING", "USER", "QUIT", "PRIVMSG",
 	NULL
 };
 
+/** The functions pointers for the various RFC commands handlers */
 const cmds G_CMDS[REF_NB] = {
 	join, nick, ping, user, quit, privmsg,
 	NULL
