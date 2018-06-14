464900e3cb4fdb5a43dedcddb9b26523a47c7e29
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-14 12:58:53 +0200

    [ServerSources] Rename channel_list to teams_list

--- server_src/teams_list.c
+++ server_src/teams_list.c
@@ -2,19 +2,19 @@
 ** EPITECH PROJECT, 2018
 ** MY_FTP
 ** File description:
-** my_ftp server channel list methods
+** my_ftp server teams list methods
 */
 
 #include "../include/server.h"
 #include <stdlib.h>
 
 /** Self explanatory
-* @param userlist the userlist to insert in the new channel element
-* @param channame the new channel name
+* @param userlist the userlist to insert in the new teams element
+* @param channame the new teams name
 */
-t_channel	*get_new_chan_list(t_userlist *userlist, char *channame)
+t_teams	*get_new_chan_list(t_userlist *userlist, char *channame)
 {
-	t_channel *list = malloc(sizeof(*list));
+	t_teams *list = malloc(sizeof(*list));
 
 	if (list) {
 		memset(list, 0, sizeof(*list));
@@ -26,13 +26,13 @@ t_channel	*get_new_chan_list(t_userlist *userlist, char *channame)
 	return (list);
 }
 
-/** Push back a new channel element to the main list container
+/** Push back a new teams element to the main list container
 * @param head the head pointer
-* @param new the new channel element to be inserted
+* @param new the new teams element to be inserted
 */
-void	*insert_back_channel(t_channel *head, t_channel *new)
+void	*insert_back_teams(t_teams *head, t_teams *new)
 {
-	t_channel *tmp = head;
+	t_teams *tmp = head;
 
 	if (!head) {
 		head = new;
@@ -46,13 +46,13 @@ void	*insert_back_channel(t_channel *head, t_channel *new)
 	return (head);
 }
 
-/** Remove a channel element from the doubly linked list
+/** Remove a teams element from the doubly linked list
 * @param list the userlist to be updated
-* @param channame the channel name to be deleted
+* @param channame the teams name to be deleted
 */
-void	remove_channel(t_channel *list, char *channame)
+void	remove_teams(t_teams *list, char *channame)
 {
-	t_channel *tmp = list;
+	t_teams *tmp = list;
 
 	while (tmp->next != NULL)
 	{
@@ -71,25 +71,25 @@ void	remove_channel(t_channel *list, char *channame)
 /** Self explanatory
 * @param list the list to be de-allocated
 */
-void	free_channel_list(t_channel *list)
+void	free_teams_list(t_teams *list)
 {
 	while (list) {
 		free_userlist(list->users);
-		t_channel *next = list->next;
+		t_teams *next = list->next;
 		free(list);
 		list = next;
 	}
 }
 
 /** Self explanatory
-* @return the default channel where all user resides
-* @note the channel name, the default user and all could be parsed from
+* @return the default teams where all user resides
+* @note the teams name, the default user and all could be parsed from
 * a config file (#pullrequestIdea)
 */
-t_channel	*init_default_channel(void)
+t_teams	*init_default_teams(void)
 {
 	char *defuser[5] = {"NICK", "4Dm1n", "42", "*", ":John Doe"};
-	t_channel *defchan = malloc(sizeof(*defchan));
+	t_teams *defchan = malloc(sizeof(*defchan));
 	t_user *admin = get_new_user(0, defuser);
 	t_userlist *allusers = get_new_userlist(admin);
 
