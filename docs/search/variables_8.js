5ff7815c18449b0c1e8bfca2996c79cc529a0f9b
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-15 22:55:34 +0200

    [Doc] Refresh doc

--- docs/search/variables_8.js
+++ docs/search/variables_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['id',['id',['../structt__inhabitant.html#a7a711ff5e17b384d16c6ac48c70d9564',1,'t_inhabitant']]],
+  ['inventory',['inventory',['../structt__inhabitant.html#a9733bf193adb75accd9fc5c777490fef',1,'t_inhabitant']]],
   ['isco',['isco',['../structt__user.html#af93924b30e62f9eec7fbf113f8051173',1,'t_user']]]
 ];
