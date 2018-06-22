13a3ce6d0ae5a600fdac28cdf2f73375d0486447
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-22 07:34:10 +0200

    [ClientGUI] Add forgotten include guards and update prop names

--- client/gui_part/include/Menu_Settings.hpp
+++ client/gui_part/include/Menu_Settings.hpp
@@ -5,6 +5,8 @@
 // Main Gui Menu Settings header
 //
 
+#ifndef MENU_SETTINGS_HPP_
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
@@ -27,19 +29,21 @@ class	MenuSettings
 	private:
 		sf::Texture		SettingsTextures;
 		sf::Sprite		SettingsSprite;
-		sf::Vector2f		SettingsPos;
+		sf::Vector2f		SettingsSize;
 		sf::RectangleShape 	SettingsRect;
 		sf::Font		SettingsFont;
 		sf::Text 		SettingsText;
-		sf::Vector2f		ServerPortSettingsPos;
+		sf::Vector2f		ServerPortSettingsSize;
 		sf::RectangleShape 	ServerPortSettingsRect;
 		sf::Font		ServerPortFont;
 		sf::Text 		ServerPortText;
 		sf::String		ServerPortInputString;
-		sf::Vector2f		ServerHostSettingsPos;
+		sf::Vector2f		ServerHostSettingsSize;
 		sf::RectangleShape 	ServerHostSettingsRect;
 		sf::Text 		ServerHostText;
 		sf::String		ServerHostInputString;
-		sf::Vector2f		HelpSettingsPos;
+		sf::Vector2f		HelpSettingsSize;
 		sf::RectangleShape 	HelpSettingsRect;
 };
+
+#endif /* !MENU_NEWGAME_HPP_ */
