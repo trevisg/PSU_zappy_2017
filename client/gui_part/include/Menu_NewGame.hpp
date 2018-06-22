78aad2becb2056c4af6719a8a45df7b13efc7abf
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-22 07:33:32 +0200

    [ClientGUI] Add forgotten include guards and click sound properties

--- client/gui_part/include/Menu_NewGame.hpp
+++ client/gui_part/include/Menu_NewGame.hpp
@@ -5,6 +5,9 @@
 // Main Gui Menu new game header
 //
 
+#ifndef MENU_NEWGAME_HPP_
+
+#include "Game_Window.hpp"
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
@@ -14,8 +17,12 @@ class	MenuNewGame
 		MenuNewGame(int xsize, int ysize);
 		sf::RectangleShape	getNewGameRect();
 		sf::Text		getNewGameText();
+		GameWindow		*getGameWindow();
 		void 			setNewGamePos(int xpos, int ypos);
 	private:
+		GameWindow		*game;
+		sf::SoundBuffer		soundbuff;
+		sf::Sound		selectsound;
 		sf::Texture		newgametextures;
 		sf::Sprite		newgamesprite;
 		sf::Vector2f		newgamesize;
@@ -23,3 +30,5 @@ class	MenuNewGame
 		sf::Font		newgamefont;
 		sf::Text 		newgametext;
 };
+
+#endif /* !MENU_NEWGAME_HPP_ */
