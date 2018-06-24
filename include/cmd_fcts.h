720010aadb34fe5c0fd79cfb1dd55c3069198530
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-24 08:11:06 +0200

    [ServerSources] Add include guards, remove comments .. cleaning

--- include/cmd_fcts.h
+++ include/cmd_fcts.h
@@ -6,57 +6,56 @@
 ** Send informations to client
 */
 
-#include "server.h"
+#ifndef CMD_FCTS_H_
+# define CMD_FCTS_H_
 
-/* send_fcts/motion.c */
-void *forward(cmdargs args, int clifd, t_world *map);
-void *right(cmdargs args, int clifd, t_world *map);
-void *left(cmdargs args, int clifd, t_world *map);
+	#include "server.h"
 
-/* send_fcts/game.c
-void *send_game_end(cmdargs args, int clifd, t_teams *teams);
-void *send_map_size(cmdargs args, int clifd, t_teams *teams);
-void *send_map_content(cmdargs args, int clifd, t_teams *teams);
-void *send_tile_content(cmdargs args, int clifd, t_teams *teams);
-*/
-/* send_fcts/player.c
-void *send_player_join(cmdargs args, int clifd, t_teams *teams);
-void *send_player_pos(cmdargs args, int clifd, t_teams *teams);
-void *send_player_lv(cmdargs args, int clifd, t_teams *teams);
-void *send_player_inv(cmdargs args, int clifd, t_teams *teams);
-void *send_player_death(cmdargs args, int clifd, t_teams *teams);
-*/
-/* send_fcts/team.c
-void *send_player_from_egg(cmdargs args, int clifd, t_teams *teams);
-void *send_team_names(cmdargs args, int clifd, t_teams *teams);
-void *send_expulsion(cmdargs args, int clifd, t_teams *teams);
-void *send_broadcast(cmdargs args, int clifd, t_teams *teams);
-*/
-/* send_fcts/time.c
-void *send_inc_start(cmdargs args, int clifd, t_teams *teams);
-void *send_inc_end(cmdargs args, int clifd, t_teams *teams);
-void *send_time_unit(cmdargs args, int clifd, t_teams *teams);
-void *send_time_modif(cmdargs args, int clifd, t_teams *teams);
-*/
-/* send_fcts/egg.c
-void *send_egg_lay(cmdargs args, int clifd, t_teams *teams);
-void *send_egg_laid(cmdargs args, int clifd, t_teams *teams);
-void *send_egg_hatch(cmdargs args, int clifd, t_teams *teams);
-void *send_egg_death(cmdargs args, int clifd, t_teams *teams);
-*/
-/* send_fcts/cmd.c
-void *send_res_drop(cmdargs args, int clifd, t_teams *teams);
-void *send_res_collect(cmdargs args, int clifd, t_teams *teams);
-void *send_msg(cmdargs args, int clifd, t_teams *teams);
-void *send_cmd_unknow(cmdargs args, int clifd, t_teams *teams);
-void *send_cmd_params(cmdargs args, int clifd, t_teams *teams);
-*/
-/* send_fcts/find_list.c
-t_user *find_user_in_teams(t_teams *teams, int clifd);
-t_user *find_player_by_id(t_teams *teams, unsigned int id);
-int get_nbr_by_level(int level);
-t_egg *find_egg_on_tile(t_tile *tile, t_user *layer);
-t_egg *find_egg_state(t_user *user, EGG_STATE state);
-*/
-/* send_fcts/utils.c */
-void move(t_inhabitant *player, t_world *map);
+	/** send_fcts/motion.c */
+	void *forward(cmdargs args, int clifd, t_world *map);
+	void *right(cmdargs args, int clifd, t_world *map);
+	void *left(cmdargs args, int clifd, t_world *map);
+
+	#ifdef CMD_ALL
+		/** see send_fcts/game.c */
+	void *send_game_end(cmdargs args, int clifd, t_teams *teams);
+	void *send_map_size(cmdargs args, int clifd, t_teams *teams);
+	void *send_map_content(cmdargs args, int clifd, t_teams *teams);
+	void *send_tile_content(cmdargs args, int clifd, t_teams *teams);
+		/** see send_fcts/player.c */
+	void *send_player_join(cmdargs args, int clifd, t_teams *teams);
+	void *send_player_pos(cmdargs args, int clifd, t_teams *teams);
+	void *send_player_lv(cmdargs args, int clifd, t_teams *teams);
+	void *send_player_inv(cmdargs args, int clifd, t_teams *teams);
+	void *send_player_death(cmdargs args, int clifd, t_teams *teams);
+		/** send_fcts/team.c */
+	void *send_player_from_egg(cmdargs args, int clifd, t_teams *teams);
+	void *send_team_names(cmdargs args, int clifd, t_teams *teams);
+	void *send_expulsion(cmdargs args, int clifd, t_teams *teams);
+	void *send_broadcast(cmdargs args, int clifd, t_teams *teams);
+		/** see send_fcts/time.c */
+	void *send_inc_start(cmdargs args, int clifd, t_teams *teams);
+	void *send_inc_end(cmdargs args, int clifd, t_teams *teams);
+	void *send_time_unit(cmdargs args, int clifd, t_teams *teams);
+	void *send_time_modif(cmdargs args, int clifd, t_teams *teams);
+		/** see send_fcts/egg.c */
+	void *send_egg_lay(cmdargs args, int clifd, t_teams *teams);
+	void *send_egg_laid(cmdargs args, int clifd, t_teams *teams);
+	void *send_egg_hatch(cmdargs args, int clifd, t_teams *teams);
+	void *send_egg_death(cmdargs args, int clifd, t_teams *teams);
+		/**  see send_fcts/cmd.c */
+	void *send_res_drop(cmdargs args, int clifd, t_teams *teams);
+	void *send_res_collect(cmdargs args, int clifd, t_teams *teams);
+	void *send_msg(cmdargs args, int clifd, t_teams *teams);
+	void *send_cmd_unknow(cmdargs args, int clifd, t_teams *teams);
+	void *send_cmd_params(cmdargs args, int clifd, t_teams *teams);
+		/** see send_fcts/find_list.c */
+	t_user *find_user_in_teams(t_teams *teams, int clifd);
+	t_user *find_player_by_id(t_teams *teams, unsigned int id);
+	int get_nbr_by_level(int level);
+	t_egg *find_egg_on_tile(t_tile *tile, t_user *layer);
+	t_egg *find_egg_state(t_user *user, EGG_STATE state);
+
+	#endif /* !CMD_ALL */
+
+#endif /* !CMD_FCTS_H_ */
