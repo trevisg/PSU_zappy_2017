96317769a389e307764b2f524d9d77be4871d1e6
cantit_b <bastien.cantiteau@epitech.eu>
2018-06-24 14:49:17 +0200

    [Server] Modification of attribute in send_fcts, teams became map

--- server/src/send_fcts/cmd.c
+++ server/src/send_fcts/cmd.c
@@ -7,51 +7,51 @@
 
 #include "cmd_fcts.h"
 
-void *send_res_drop(cmdargs args, int clifd, t_teams *teams)
+void *send_res_drop(cmdargs args, int clifd, t_world *map)
 {
-	t_user *user = find_user_in_teams(teams, clifd);
+	t_user *user = find_user_in_teams(map->teams, clifd);
 
-	for (t_teams tmpt = teams; tmpt; tmpt = tmpt->next) {
+	for (t_teams tmpt = map->teams; tmpt; tmpt = tmpt->next) {
 		for (t_userlist tmpu = tmpu->users; tmpu; tmpu = tmpu->next) {
 			dprintf(tmpu->user->clifd, "pdr %d %d\n",
 				tmpu->user->refplayer->id,
 				tmpu->user->refplayer->just_drop);
 		}
 	}
-	return teams;
+	return map;
 }
 
-void *send_res_collect(cmdargs args, int clifd, t_teams *teams)
+void *send_res_collect(cmdargs args, int clifd, t_world *map)
 {
-	t_user *user = find_user_in_teams(teams, clifd);
+	t_user *user = find_user_in_teams(map->teams, clifd);
 
-	for (t_teams tmpt = teams; tmpt; tmpt = tmpt->next) {
+	for (t_teams tmpt = map->teams; tmpt; tmpt = tmpt->next) {
 		for (t_userlist tmpu = tmpu->users; tmpu; tmpu = tmpu->next) {
 			dprintf(tmpu->user->clifd, "pgt %d %d\n",
 				tmpu->user->refplayer->id,
 				tmpu->user->refplayer->just_collect);
 		}
 	}
-	return teams;
+	return map;
 }
 
-void *send_msg(cmdargs args, int clifd, t_teams *teams)
+void *send_msg(cmdargs args, int clifd, t_world *map)
 {
-	for (t_teams *tmpt = teams; tmpt; tmpt = tmpt->next) {
+	for (t_teams *tmpt = map->teams; tmpt; tmpt = tmpt->next) {
 		for (t_userlist *tmpu = tmpt->users; tmpu; tmpu = tmpu->next) {
 			dprintf(tmpu->user->clifd, "smg %s\n", args[0]);
 		}
 	}
-	return teams;
+	return map;
 }
 
 /* Provide the clifd of the requester */
-void *send_cmd_unknow(cmdargs args, int clifd, t_teams *teams)
+void *send_cmd_unknow(cmdargs args, int clifd, t_world *map)
 {
 	dprintf(clifd, "suc\n");
 }
 
-void *send_cmd_params(cmdargs args, int clifd, t_teams *teams)
+void *send_cmd_params(cmdargs args, int clifd, t_world *map)
 {
 	dprintf(clifd, "sbp\n");
 }
--- server/src/send_fcts/game.c
+++ server/src/send_fcts/game.c
@@ -7,50 +7,50 @@
 
 #include "cmd_fcts.h"
 
-void *send_game_end(cmdargs args, int clifd, t_teams *teams)
+void *send_game_end(cmdargs args, int clifd, t_world *map)
 {
-	dprintf(clifd, "seg %s\n", teams->team_name);
-	return teams;
+	dprintf(clifd, "seg %s\n", map->teams->team_name);
+	return map;
 }
 
-void *send_map_size(cmdargs args, int clifd, t_teams *teams)
+void *send_map_size(cmdargs args, int clifd, t_world *map)
 {
-	dprintf(clifd, "msz %d %d\n", teams->refmap->sizeX, teams->refmap->sizeY);
-	return teams;
+	dprintf(clifd, "msz %d %d\n", map.sizeX, map.sizeY);
+	return map;
 }
 
-void *send_map_content(cmdargs args, int clifd, t_teams *teams)
+void *send_map_content(cmdargs args, int clifd, t_world *map)
 {
 	int i;
 	int j;
 	
-	for (i = 0; i < teams->refmap->sizeY; i++) {
-		for (j = 0; j < teams->refmap->sizeX; j++) {
+	for (i = 0; i < map.sizeY; i++) {
+		for (j = 0; j < map.sizeX; j++) {
 			dprintf(clifd, "btc %d %d %d %d %d %d %d %d %d", j, i,
-				teams->refmap->tiles[i][j].ressources.food.qtt,
-				teams->refmap->tiles[i][j].ressources.linemate.qtt,
-				teams->refmap->tiles[i][j].ressources.deraumere.qtt,
-				teams->refmap->tiles[i][j].ressources.sibur.qtt,
-				teams->refmap->tiles[i][j].ressources.mendiane.qtt,
-				teams->refmap->tiles[i][j].ressources.phiras.qtt,
-				teams->refmap->tiles[i][j].ressources.thystame.qtt);
+				map->tiles[i][j].ressources.food.qtt,
+				map->tiles[i][j].ressources.linemate.qtt,
+				map->tiles[i][j].ressources.deraumere.qtt,
+				map->tiles[i][j].ressources.sibur.qtt,
+				map->tiles[i][j].ressources.mendiane.qtt,
+				map->tiles[i][j].ressources.phiras.qtt,
+				map->tiles[i][j].ressources.thystame.qtt);
 		}
 	}
-	return teams;
+	return map;
 }
 
-void *send_tile_content(cmdargs args, int clifd, t_teams *teams)
+void *send_tile_content(cmdargs args, int clifd, t_world *map))
 {
 	int x = atoi(args[0]);
 	int y = atoi(args[1]);
 	
 	dprintf(clifd, "btc %d %d %d %d %d %d %d %d %d\n", x, y,
-		teams->refmap->tiles[x][y].ressources.food.qtt,
-		teams->refmap->tiles[x][y].ressources.linemate.qtt,
-		teams->refmap->tiles[x][y].ressources.deraumere.qtt,
-		teams->refmap->tiles[x][y].ressources.sibur.qtt,
-		teams->refmap->tiles[x][y].ressources.mendiane.qtt,
-		teams->refmap->tiles[x][y].ressources.phiras.qtt,
-		teams->refmap->tiles[x][y].ressources.thystame.qtt);
-	return teams;
+		map->tiles[x][y].ressources.food.qtt,
+		map->tiles[x][y].ressources.linemate.qtt,
+		map->tiles[x][y].ressources.deraumere.qtt,
+		map->tiles[x][y].ressources.sibur.qtt,
+		map->tiles[x][y].ressources.mendiane.qtt,
+		map->tiles[x][y].ressources.phiras.qtt,
+		map->tiles[x][y].ressources.thystame.qtt);
+	return map;
 }
