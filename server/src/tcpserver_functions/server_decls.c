d163e0b2ab1044045276d9f56a43595861c8c8e3
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-26 15:06:09 +0200

    [ServerSources] Ident fix

--- server/src/tcpserver_functions/server_decls.c
+++ server/src/tcpserver_functions/server_decls.c
@@ -10,14 +10,28 @@
 
 /** The 2D array containing the handled commands strings for comparison  */
 const char *G_PROTOS[REF_NB] = {
-	"Forward", "Right", "Left", "Team", "Inventory", "Broadcast",
-	"Connect_nbr", "Take", "Set",
+	"Forward",
+	"Right",
+	"Left",
+	"Team",
+	"Inventory",
+	"Broadcast",
+	"Connect_nbr",
+	"Take",
+	"Set",
 	NULL
 };
 
 /** The functions pointers for the various RFC commands handlers */
 const cmds G_CMDS[REF_NB] = {
-	r_forward, r_right, r_left, r_team, r_inventory, r_broadcast,
-	r_connect_nbr, r_take_obj, r_set_obj,
+	r_forward,
+	r_right,
+	r_left,
+	r_team,
+	r_inventory,
+	r_broadcast,
+	r_connect_nbr,
+	r_take_obj,
+	r_set_obj,
 	NULL
 };
