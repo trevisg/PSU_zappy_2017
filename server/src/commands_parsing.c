a8310d2c868ca8c2689812ed19d585868b757161
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-26 23:55:49 +0200

    [ServerSources #7] Split function pointer usage

--- server/src/commands_parsing.c
+++ server/src/commands_parsing.c
@@ -5,7 +5,7 @@
 ** Zappy server commands parsing functions source file
 */
 
-#include "server.h"
+#include "../include/server.h"
 #include <stdlib.h>
 
 /** Split the received buffer by the '\\r\\n' symbol to get multiple
@@ -33,7 +33,7 @@ static char	**get_commands(char *req)
 /** Tokenize the command buffer by splitting by spaces
 * @param cmd a command string received after get_commands() tokenization
 */
-static char 	**get_args(char *cmd)
+static char 	**get_args(char *cmd, const char *sep)
 {
 	int i = 0;
 	char *token = NULL;
@@ -41,14 +41,53 @@ static char 	**get_args(char *cmd)
 
 	if (tkns) {
 		memset(tkns, 0, sizeof(*tkns) * MAXARGS);
-		token = strtok(cmd, " :");
+		token = strtok(cmd, sep);
 		while (token) {
 			tkns[i] = strdup(token);
-			token = strtok(NULL, " :");
+			token = strtok(NULL, sep);
 			i = i <= MAXARGS ? i + 1 : i;
 		}
 	}
 	return (tkns);
+}
+
+/** Run command for AI client type
+* @param args the 2D char array containing the request splitted by spaces
+*/
+void	run_ia_cmd(char *cmd, int clifd, t_world *map)
+{
+	char **args = NULL;
+	uint index = 0;
+
+	for (index = 0; G_PROTOS[index]; ++index) {
+		if ((strstr(cmd, G_PROTOS[index]))) {
+			args = get_args(cmd, " :");
+			G_CMDS[index](args, clifd, map);
+			free_buffers(args);
+			break;
+		}
+	}
+}
+
+/** Run command for Graphical client type
+* @param cmd to check for
+* @param clifd the client file descriptor to read on
+* @param map the t_world trantor map
+* @param index the
+*/
+void	run_graph_cmd(char *cmd, int clifd, t_world *map)
+{
+	char **args = NULL;
+	int index = 0;
+
+	for (index = 0; G_GRAPH_PROTO[index]; ++index) {
+		if ((strstr(cmd, G_GRAPH_PROTO[index]))) {
+			args = get_args(cmd, " /&=");
+			G_GRAPH_CMDS[index](args, clifd, map);
+			free_buffers(args);
+			break;
+		}
+	}
 }
 
 /** Match all methods in G_CMDS for a given req command string
@@ -61,17 +100,10 @@ int	get_methods(char *req, int clifd, t_world *map)
 {
 	int index = 0;
 	char **cmds = get_commands(req);
-	char **args = NULL;
 
 	for (int j = 0; cmds[j]; ++j) {
-		for (index = 0; G_PROTOS[index]; ++index) {
-			if ((strstr(cmds[j], G_PROTOS[index]))) {
-				args = get_args(cmds[j]);
-				G_CMDS[index](args, clifd, map);
-				free_buffers(args);
-				break;
-			}
-		}
+		run_ia_cmd(cmds[j], clifd, map);
+		run_graph_cmd(cmds[j], clifd, map);
 	}
 	free_buffers(cmds);
 	return ((index == REF_NB) ? (0) : (index));
