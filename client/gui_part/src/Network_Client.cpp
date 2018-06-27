87582aa3d8112ecf3dad9e6e2d8d4b0316957226
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-27 15:31:16 +0200

    [graphClient/Server] New start

--- client/gui_part/src/Network_Client.cpp
+++ client/gui_part/src/Network_Client.cpp
@@ -19,7 +19,7 @@ Network::~Network()
 	close(_client_socket);
 }
 
-bool	Network::connect_to(std::string server_host, std::string server_port)
+bool	Network::_connect_to(std::string server_host, std::string server_port)
 {
 	int rt = 0;
 	uint address_size = sizeof(_client_address);
@@ -38,24 +38,89 @@ bool	Network::connect_to(std::string server_host, std::string server_port)
 	return (rt > 0 ? true : false);
 }
 
-std::vector<std::string>	Network::get_teamnames()
+std::vector<std::string>	Network::_get(std::string req)
 {
 	std::vector<std::string> teams;
-	std::string req("GET /teamnames\n");
-	write(_client_socket, req.c_str(), req.size());
-	listen_up();
-	while (!_queue.empty()) {
-		teams.emplace_back(_queue.front());
+	std::string request("GET /" + req + "\n");
+	std::cerr << "Sending request : " << request;
+	write(_client_socket, request.c_str(), request.size());
+	_listen_up();
+	if (_queue.front() == req) {
 		_queue.pop();
+		while (!_queue.empty()) {
+			teams.emplace_back(_queue.front());
+			_queue.pop();
+		}
+	} else {
+		teams.emplace_back("\nERROR invalid format");
 	}
 	return (teams);
 }
 
-bool	Network::listen_up()
+std::map<std::string, int>	Network::_get_mapsize()
+{
+	std::map<std::string, int> mapsize;
+	std::string request("GET /mapsize\n");
+	std::cerr << "Sending request : " << request;
+	write(_client_socket, request.c_str(), request.size());
+	_listen_up();
+	if (_queue.front() == "mapsize") {
+		_queue.pop();
+		while (!_queue.empty()) {
+			std::string index(_queue.front());
+			mapsize[index] = 0;
+			_queue.pop();
+			uint val = std::stoi (_queue.front(), nullptr, 10);
+			mapsize[index] = val;
+			_queue.pop();
+		}
+	} else {
+		mapsize["ERROR"] = -1;
+	}
+	return (mapsize);
+}
+
+std::vector<std::map<std::string, std::string> >
+			Network::_get_team_members(std::string teamname)
+{
+	uint 	members = 0;
+	std::map<std::string, std::string> teams_members;
+	std::vector<std::map<std::string, std::string> > teams;
+	std::string request("POST /team_detail&team=" + teamname + "\n");
+
+	std::cerr << "Sending request : " << request;
+	write(_client_socket, request.c_str(), request.size());
+	_listen_up();
+	_listen_up();
+	_queue.pop();
+	while (!_queue.empty()) {
+		std::string index;
+		std::string value;
+
+		index = _queue.front();
+		_queue.pop();
+		value = _queue.front();
+		teams_members[index] = value;
+		if (teams_members.find("id") != teams_members.end()
+		&& teams_members.find("x") != teams_members.end()
+		&& teams_members.find("y") != teams_members.end()
+		&& teams_members.find("ttl") != teams_members.end()) {
+			teams_members["id"] = std::to_string(members);
+			teams.emplace_back(teams_members);
+			teams_members.clear();
+			members += 1;
+		}
+		if (!_queue.empty())
+			_queue.pop();
+	}
+	return (teams);
+}
+
+bool	Network::_listen_up()
 {
 	int r_size = 0;
 	std::string s;
-	std::regex r("[a-z]+");
+	std::regex r("[a-z0-9.]+");
 
 	memset(_server_resp, 0, MAX_RESP);
 	while ((r_size = read(_client_socket, _server_resp, MAX_RESP)) > 0) {
@@ -67,13 +132,7 @@ bool	Network::listen_up()
 				_queue.push(m.str());
 			}
 			break;
-		} else
-			break;
+		}
 	}
 	return (r_size > 0 ? true : false);
-}
-
-std::string	Network::get_lastbuffer()
-{
-	return _queue.front();
 }
