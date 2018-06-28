0062a675443eae1e3d47a0029b856208a13786c8
Trevis Gulby <trevisgulby@protonmail.ch>
2018-06-28 13:37:35 +0200

    [Doc] Refresh doc

--- docs/search/pages_3.js
+++ docs/search/pages_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['zappy_20graphclient_20to_20server_20protocol',['Zappy GraphClient to Server protocol',['../md_bonus_GRAPH_API_ENDPOINTS.html',1,'']]],
-  ['zappy_20iaclient_20to_20server_20protocol',['Zappy IAClient to Server protocol',['../md_bonus_ZAPPY_CMDS.html',1,'']]]
+  ['zappy_20aiclient_20to_20server_20protocol',['Zappy AIClient to Server protocol',['../md_bonus_ZAPPY_CMDS.html',1,'']]]
 ];
