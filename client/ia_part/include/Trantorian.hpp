6f23a3b86de3f80d8eedc3663c9a2b1a5cd9f7f8
Trevis Gulby <trevisgulby@protonmail.ch>
2018-07-01 18:56:16 +0200

    [AI/Doc] Update AI documentation

--- client/ia_part/include/Trantorian.hpp
+++ client/ia_part/include/Trantorian.hpp
@@ -14,11 +14,22 @@
 #include <map>
 #include "../../commons/include/Network_Client.hpp"
 
+/** The life a player */
+#define TTL 126
+/** If max player is reachedn, this is the exit code */
+#define EXIT_MAX_PLAYER 84
+/** No comment */
+#define EVER ;;
+/** Simple debugging trick to have one action per f second */
+#define Frequency(f) sleep(f);
+
+/** To get positions in int array or vector */
 enum {
 	X = 0,
 	Y = 1
 };
 
+/** The AI states */
 typedef enum  {
 	DROPPING = 0,
 	FORKING = 1,
@@ -28,6 +39,7 @@ typedef enum  {
 	PICKING_STONE = 5
 }	STATE;
 
+/** The main AI class */
 class Trantorian
 {
 	public:
@@ -35,14 +47,18 @@ class Trantorian
 		~Trantorian();
 		bool	check_co();
 		void 	_do_fork();
+		void 	_do_moove();
+		bool	_do_drop();
 		bool	run(std::string team_name);
-		void	_do_search(std::vector<std::string>  res, uint cnt);
+		void	_do_search(uint cnt);
 	private:
 		Network				_client;
-		uint					_level;
-		std::string		_team_name;
-		std::string		_server_port;
-		std::string		_server_host;
+		uint				_level;
+		std::vector<int>		_map_size;
+		std::string			_team_name;
+		STATE 				_curr_state;
+		std::string			_server_port;
+		std::string			_server_host;
 };
 
 #endif /* !TRANTORIAN_HPP_ */
--- client/ia_part/src/Trantorian.cpp
+++ client/ia_part/src/Trantorian.cpp
@@ -7,11 +7,10 @@
 
 #include "../include/Trantorian.hpp"
 
-#define TTL 126
-#define EXIT_MAX_PLAYER 84
-#define EVER ;;
-#define Frequency(f) sleep(f);
-
+/** The AI constructor
+* @param server_port the zappy_server port
+* @param server_host the zappy_server address
+*/
 Trantorian::Trantorian(std::string server_port, std::string server_host) :
 _server_port(server_port), _server_host(server_host)
 {
@@ -23,6 +22,10 @@ Trantorian::~Trantorian()
 
 }
 
+/** Utility function around the Trantorian::_client connect_to method
+* @return true if connection succedded
+* @return false if not
+*/
 bool	Trantorian::check_co()
 {
 	bool rt = false;
@@ -30,54 +33,10 @@ bool	Trantorian::check_co()
 	return (rt);
 }
 
-void 	log_drop( std::map<std::string, std::string> resp,
-		std::vector<int> map_size, std::string team_name)
-{
-	std::cout << "Finished drop on this " << map_size[X]
-	<< "*" << map_size[Y] << " map\n";
-	std::cout << "Note : Still have ["
-	<< resp["PLAYER_CREDIT"] << "] trantorians to drop "
-	<< "for team [\"" << team_name << "\"]\n";
-}
-
-void	Trantorian::_do_search(std::vector<std::string>  res, uint  cnt)
-{
-	std::cerr << "state : " << "SEARCHING\n";
-	std::cerr << "send -->look \n";
-	std::cerr << "Receive :\n";
-	for (uint i = 0; i != res.size(); ++i) {
-		std::cerr << "[" << res[i] << "]\n";
-	}
-	if (cnt >= TTL) {
-		std::cerr << "No more life exiting..\n";
-		kill(getpid(), SIGINT);
-	}
-}
-
-void	Trantorian::_do_fork()
-{
-	static uint childs;
-
-	childs += 1;
-	if (fork() == 0) {
-		std::cerr << " Where in child n°" << childs << '\n';
-		Trantorian ia_player(_server_port, _server_host);
-		bool check = ia_player.check_co();
-		if (check) {
-			ia_player.run(_team_name);
-		}
-	}
-}
-
-void	do_moove(int _level)
-{
-	std::cerr << "state : " << "MOOVING\n";
-	std::cerr << "Send Forward on trantor map \n";
-	std::cerr << "Note : we're still on level [";
-	std::cerr << _level << "]\n";
-	std::cerr << "send -->Forward \n";
-}
-
+/** Utility function to print the current AI STATE and the TimeTolive value
+* @param cnt the time(aka life) counter
+* @param value the current ai STATE value
+*/
 void 	print_state(uint cnt, STATE value)
 {
 
@@ -105,52 +64,135 @@ void 	print_state(uint cnt, STATE value)
 	std::cerr << "TimeTolive: " << TTL - cnt << "s\n";
 }
 
+/** Utility function to log a new ai player loading and connection
+* @param resp the response from server containing the map size and the
+* number of players from the team leaving to be connected
+* @param map_size 0 x pos, 1 y pos
+* @param team_name the current ai player team name
+*/
+void 	log_drop( std::map<std::string, std::string> resp,
+		std::vector<int> map_size, std::string team_name)
+{
+	std::cout << "Finished drop on this " << map_size[X]
+	<< "*" << map_size[Y] << " map\n";
+	std::cout << "Note : Still have ["
+	<< resp["PLAYER_CREDIT"] << "] trantorians to drop "
+	<< "for team [\"" << team_name << "\"]\n";
+}
+
+/** The DROPPING state method , send `Teams [teamname]` command to server
+* and parse response. Exit if MAX_PLAYER per team value is reached
+*/
+bool	Trantorian::_do_drop()
+{
+	bool rt = false;
+	std::map<std::string, std::string> resp;
+
+	resp = _client.get_map_dimension(_team_name);
+	if (resp.find("x") != resp.end()
+	&& resp.find("y") != resp.end()
+	&& resp.find("PLAYER_CREDIT") != resp.end()) {
+		_map_size.push_back(std::stoi (resp["x"], nullptr, 10));
+		_map_size.push_back(std::stoi (resp["y"], nullptr, 10));
+		log_drop(resp, _map_size, _team_name);
+		rt = true;
+	} else if (resp.find("ko") != resp.end()) {
+		std::cerr << "Error : reaching MAX PLAYER for team "
+		<< _team_name << "\n";
+		exit(EXIT_MAX_PLAYER);
+	}
+	return (rt);
+}
+
+/** The SEARCHING state method, send `Look` command to server and (not yet)
+* parse the response
+*/
+void	Trantorian::_do_search(uint cnt)
+{
+	std::vector<std::string> res = _client.look();
+	std::cerr << "state : " << "SEARCHING\n";
+	std::cerr << "send -->look \n";
+	std::cerr << "Receive :\n";
+	for (uint i = 0; i != res.size(); ++i) {
+		std::cerr << "[" << res[i] << "]\n";
+	}
+	if (cnt >= TTL) {
+		std::cerr << "No more life exiting..\n";
+		kill(getpid(), SIGINT);
+	}
+}
+
+/** The SEARCHING state method, send `Fork` command to server and if server
+* respond `ok`, call fork() and launch a new Trantorian teammate in the child
+* process
+*/
+void	Trantorian::_do_fork()
+{
+	static uint childs;
+	std::vector<std::string> resp;
+
+	childs += 1;
+	resp = _client.fork();
+	if (fork() == 0) {
+		std::cerr << " Where in child n°" << childs << '\n';
+		Trantorian ia_player(_server_port, _server_host);
+		bool check = ia_player.check_co();
+		if (check) {
+			ia_player.run(_team_name);
+		}
+	}
+}
+
+void	Trantorian::_do_moove()
+{
+	std::map<std::string, std::string> resp;
+
+	std::cerr << "state : " << "MOOVING\n";
+	std::cerr << "Send Forward on trantor map \n";
+	std::cerr << "Note : we're still on level [";
+	std::cerr << _level << "]\n";
+	resp = _client.forward();
+	std::cerr << "send -->Forward \n";
+}
+
+/** The main AI event loop
+* @param team_name the new Trantorian team name
+* @note this stupid AI do only one thing, forward,
+* forward and forward again until death
+*/
 bool	Trantorian::run(std::string team_name)
 {
 	bool rt = false;
-	STATE curr_state = DROPPING;
 	static uint cnt = 0;
-	std::vector<int> map_size;
-	std::map<std::string, std::string> resp;
 
+	_curr_state = DROPPING;
 	_team_name = team_name;
 	for (EVER) {
 		Frequency(1);
+		switch (_curr_state) {
+			case DROPPING:
+				rt = _do_drop();
+				_curr_state = SEARCHING;
+			break;
+			case SEARCHING:
+				_do_search(cnt);
+				_curr_state = MOOVING;
+			break;
+			case MOOVING:
+				_do_moove();
+				_curr_state = cnt == 30 ? FORKING : SEARCHING;
+			break;
+			case FORKING:
+				_do_fork();
+				_curr_state = SEARCHING;
+			break;
+			case PICKING_FOOD:
+			break;
+			case PICKING_STONE:
+			break;
+		}
 		++cnt;
-		resp = curr_state == DROPPING ?
-			_client.get_map_dimension(_team_name) : resp;
-		if (resp.find("x") != resp.end()
-		&& resp.find("y") != resp.end()
-		&& resp.find("PLAYER_CREDIT") != resp.end()
-		&& curr_state == DROPPING) {
-			map_size.push_back(std::stoi (resp["x"], nullptr, 10));
-			map_size.push_back(std::stoi (resp["y"], nullptr, 10));
-			log_drop(resp, map_size, team_name);
-			curr_state = SEARCHING;
-			rt = true;
-		} else if (resp.find("ko") != resp.end()) {
-			std::cerr << "Error : reaching MAX PLAYER for team "
-			<< team_name << "\n";
-			exit(EXIT_MAX_PLAYER);
-		}
-		if (curr_state == SEARCHING) {
-			std::vector<std::string> res = _client.look();
-			_do_search(res, cnt);
-			curr_state = MOOVING;
-		}
-		if (curr_state == MOOVING) {
-			std::map<std::string, std::string> resp;
-			resp = _client.forward();
-			do_moove(_level);
-			curr_state = cnt == 30 ? FORKING : SEARCHING;
-		}
-		if (curr_state == FORKING) {
-			std::vector<std::string> resp;
-			resp = _client.fork();
-			_do_fork();
-			curr_state = SEARCHING;
-		}
-		print_state(cnt, curr_state);
+		print_state(cnt, _curr_state);
 	}
 	return (rt);
 }
