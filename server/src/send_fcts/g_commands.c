37c0a29a7c458c9d620acf78f88ac7dac4599efb
Trevis Gulby <trevisgulby@protonmail.ch>
2018-07-01 11:07:04 +0200

    [SERVER] Making GraphAPI JSON compliant

--- server/src/send_fcts/g_commands.c
+++ server/src/send_fcts/g_commands.c
@@ -5,27 +5,30 @@
 ** zappy_server graphical client <-> server commands
 */
 
+#include <unistd.h>
 #include "../../include/cmd_fcts.h"
 
 void	*g_mapsize(char **args, int clifd, t_world *map)
 {
 	if (args || !args) {
-		dprintf(clifd, "{mapsize: {x: %d, y: %d}}\n",
+		dprintf(clifd, "{\"mapsize\": {\"x\": %d, \"y\": %d} }\n",
 		map->sizeX, map->sizeY);
 	}
+	close(clifd);
 	return (map);
 }
 
 void	*g_teams_names(char **args, int clifd, t_world *map)
 {
-	dprintf(clifd, "{teamnames: [");
+	dprintf(clifd, "{\"teamnames\": [");
 	if (args || !args) {
 		for (t_teams *tmp = map->teams; tmp; tmp = tmp->next) {
-			dprintf(clifd, "%s%c", tmp->team_name,
-				tmp->next ? ',' : ']');
+			dprintf(clifd, "\"%s\"%s", tmp->team_name,
+				tmp->next ? ", " : "]");
 		}
 		dprintf(clifd, "}\n");
 	}
+	close(clifd);
 	return (map);
 }
 
@@ -33,29 +36,30 @@ void	send_details(int clifd, t_userlist *teams_members)
 {
 	uint id = 0;
 	for (t_userlist *tmp = teams_members; tmp; tmp = tmp->next) {
-		dprintf(clifd, "[{id:%d,x:%d,", id, tmp->user->curr_pos[X]);
-		dprintf(clifd, "y:%d,ttl:%0.3f}]%s", tmp->user->curr_pos[Y],
-		tmp->user->ttl, tmp->next ? "," : "\n");
+		dprintf(clifd, "{\"id\": %d , \"x\": %d , ",
+		id, tmp->user->curr_pos[X]);
+		dprintf(clifd, "\"y\":%d , \"ttl\": %0.3f}%s",
+		tmp->user->curr_pos[Y], tmp->user->ttl,
+		tmp->next ? ", " : "\n");
 
 	}
 }
 
 void	*g_team_detail(char **args, int clifd, t_world *map)
 {
-	uint i = 0;
+	uint i = get_size(args);
 
-	for (; args[i]; ++i) {
-	}
-	if (i == 4 && !strcmp("team", args[2]) &&
+	if (i >= 4 && !strcmp("team", args[2]) &&
 		strlen(args[3]) <= MAX_TEAM_NAME) {
 		for (t_teams *tmp = map->teams; tmp; tmp = tmp->next) {
 			if (strstr(args[3], tmp->team_name)) {
-				dprintf(clifd, "{%s:{", args[3]);
+				dprintf(clifd, "{\"%s\":[", args[3]);
 				send_details(clifd, tmp->users);
-				dprintf(clifd, "}}\n");
+				dprintf(clifd, "]}\n");
 				break;
 			}
 		}
 	}
+	close(clifd);
 	return (map);
 }
